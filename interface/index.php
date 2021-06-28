0421d9a1e994e6cf2f155d1a39b6729fcf522ddb
ataumo <34421892+ataumo@users.noreply.github.com>
2021-06-28 19:21:34 +0200

    no index meta

--- interface/index.php
+++ interface/index.php
@@ -2,6 +2,8 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <!-- No index meta -->
+    <meta name="robots" content="noindex">
     <title>Hardening Interface</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">

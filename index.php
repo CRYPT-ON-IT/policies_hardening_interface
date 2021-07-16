6324c971645bbb1d3944288e7647ae3127660143
ataumo <l.coumau@yahoo.fr>
2021-07-16 15:20:06 +0200

    Update button position

--- index.php
+++ index.php
@@ -38,6 +38,8 @@ $link_style_master = "style/master.css";
               <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
             </svg>
           </a>
+        </div>
+        <div class="d-flex justify-content-end m-2">
           <a class="btn btn-success mx-1" role="button" href="<?php echo $link_macos_hard_interface; ?>">
             Go to the macOS Hardening interface
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-fill" viewBox="0 0 16 16">

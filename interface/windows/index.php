44c9b575ad37edd29a521fe53fa924db8be08cb1
ataumo <l.coumau@yahoo.fr>
2021-07-29 11:53:28 +0200

    Add comments

--- interface/windows/index.php
+++ interface/windows/index.php
@@ -22,11 +22,13 @@
       <div class="container mt-5" style="max-width:940px;">
         <h1>Welcome to the Windows Hardening Interface</h1>
         <hr>
+          <!-- Buttons part -->
           <div class="d-flex justify-content-end m-2">
             <!-- <a id="csv-generate" class="btn btn-success mx-1" role="button">
               <span id="csv-generate-loader" class="spinner-border spinner-border-sm" role="status" aria-hidden="true" style="display:none;"></span>
               Generate CSV File
             </a> -->
+            <!-- Generate button -->
             <a id="csv-generate" class="btn btn-success mx-1" role="button">
               <span id="csv-generate-loader" class="spinner-border spinner-border-sm" role="status" aria-hidden="true" style="display:none;"></span>
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-clockwise" viewBox="0 0 16 16">
@@ -35,6 +37,7 @@
               </svg>
               Generate CSV File
             </a>
+            <!-- Download button -->
             <a id="csv-download" class="btn btn-success mx-1" role="button">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-download" viewBox="0 0 16 16">
                 <path d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/>
@@ -45,7 +48,9 @@
             <!--download="hardening-kitty.csv" href="data:application/csv;charset=utf-8,Error"-->
           </div>
 
+          <!-- Alert part -->
           <div class="position-fixed top-0 end-0 p-3 " style="z-index: 5">
+            <!-- Downloaded alert -->
             <div id="liveToast" class="toast fade hide blur-backg" role="alert" aria-live="assertive" aria-atomic="true">
               <div class="toast-header">
                 <strong class="me-auto">Your file has been genrerated !</strong>
@@ -60,6 +65,7 @@
 
 
         <div class="row">
+          <!--////////////////////////// Nav list ///////////////////////////-->
           <div class="col-3">
             <nav id="nav-list" class="sticky-top list-group" style="top:50px;border-radius: 1em;">
               <?php include 'categories.php';?>
@@ -67,6 +73,7 @@
           </div>
 
 
+          <!--///////////////////////// Tab content /////////////////////////-->
           <div class="col-9">
 
             <!-- <div class="alert alert-success" style="width:200px">
@@ -147,6 +154,7 @@
 
 
 
+      <!--///////////////////////////// Footer //////////////////////////////-->
       <footer class="pt-5 my-5 text-muted border-top">
         Powered by | <a href="https://github.com/0x6d69636b/windows_hardening">HardeningKitty</a> | <a href="https://getbootstrap.com">Bootstrap</a> | <a href="https://cryptonit.fr">Crypt.on it</a>
       </footer>

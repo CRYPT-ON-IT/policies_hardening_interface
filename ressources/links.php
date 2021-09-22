68830bd757541eae9b6618accd4321f0957b739e
ataumo <34421892+ataumo@users.noreply.github.com>
2021-09-22 17:29:50 +0200

    Add comments in links motor

--- ressources/links.php
+++ ressources/links.php
@@ -1,14 +1,18 @@
 <?php
 
-$link_style_master = "/policies_hardening_interface/style/master.css";
-
+// BOOTSTRAP
 $link_style_bootstrap="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css";
 $style_bootstrap_intregrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x";
 
 $link_script_bootstrap="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js";
 $script_bootstrap_intregrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4";
 
+// AJAX
 $link_script_ajax="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js";
+
+// STYLE
+$link_style_master = "/policies_hardening_interface/style/master.css";
+
 // PHP code (motor)
 $link_motor_breadcrumb = $_SERVER['DOCUMENT_ROOT']."/policies_hardening_interface/ressources/breadcrumb.php";
  ?>

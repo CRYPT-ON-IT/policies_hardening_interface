ddde43a58e33695f59b32d66b0f0d6fe6259a0d9
ataumo <34421892+ataumo@users.noreply.github.com>
2021-09-28 18:37:37 +0200

    Add some comments

--- interface/windows/index.php
+++ interface/windows/index.php
@@ -1,6 +1,8 @@
 
+<!--file that will be read in interface (ux list)-->
 <?php $link_csv_file = "data/finding_list_machine_UIX.csv"; ?>
-<?php $link_csv_original_file = "data/finding_list_0x6d69636b_machine.csv" ?>
+<!--file that will be read in to generate csv file (original list)-->
+<?php $link_csv_original_file = "data/finding_list_0x6d69636b_machine.csv"; ?>
 
 <!-- All links -->
 <?php include($_SERVER['DOCUMENT_ROOT']."/policies_hardening_interface/ressources/links.php");?>
--- interface/windows/server/2016/index.php
+++ interface/windows/server/2016/index.php
@@ -1,5 +1,8 @@
 
+<!--file that will be read in interface (ux list)-->
 <?php $link_csv_file = "finding_list_cis_microsoft_windows_server_2016_1607_1.2.0_machine.csv"; ?>
+<!--file that will be read in to generate csv file (original list)-->
+<?php $link_csv_original_file = "finding_list_cis_microsoft_windows_server_2016_1607_1.2.0_machine.csv"; ?>
 
 <!-- All links -->
 <?php include($_SERVER['DOCUMENT_ROOT']."/policies_hardening_interface/ressources/links.php");?>

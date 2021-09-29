d08a243ab3c76ea88c12b47981fff9f1ae8d4646
ataumo <34421892+ataumo@users.noreply.github.com>
2021-09-29 11:16:48 +0200

    Add references values

--- interface/windows/index.php
+++ interface/windows/index.php
@@ -4,6 +4,13 @@
 <!--file that will be read in to generate csv file (original list)-->
 <?php $link_csv_original_file = "data/finding_list_0x6d69636b_machine.csv"; ?>
 
+<?php
+$reference_name = "0x6d69636b"; //* necessary value
+$reference_date = "date";       // optional
+$reference_version = "version"; // optional
+$reference_type = "machine";    // optional
+ ?>
+
 <!-- All links -->
 <?php include($_SERVER['DOCUMENT_ROOT']."/policies_hardening_interface/ressources/links.php");?>
 

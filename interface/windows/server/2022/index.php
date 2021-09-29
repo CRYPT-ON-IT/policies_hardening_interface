1242245d3bd5f520135f9dd7767a7fcb29fd58b5
ataumo <34421892+ataumo@users.noreply.github.com>
2021-09-29 18:38:40 +0200

    Update 2022 dir

--- interface/windows/server/2022/index.php
+++ interface/windows/server/2022/index.php
@@ -1,18 +1,6 @@
 
-<!--file that will be read in interface (ux list)-->
-<?php $link_csv_file = "finding_list_msft_security_baseline_windows_server_2022_dc_machine.csv"; ?>
-<!--file that will be read in to generate csv file (original list)-->
-<?php $link_csv_original_file = "finding_list_msft_security_baseline_windows_server_2022_dc_machine.csv"; ?>
-
-<?php
-$reference_name = "Department of Defence"; //* necessary value
-//$reference_date = "01/14/2020";       // optional
-$reference_version = "v2r1"; // optional
-$reference_type = "machine";    // optional
- ?>
-
 <!-- All links -->
 <?php include($_SERVER['DOCUMENT_ROOT']."/policies_hardening_interface/ressources/links.php");?>
 
 <!DOCTYPE html>
-<?php include($link_motor_csv_reader);?>
+<?php include($link_motor_version_reader); ?>

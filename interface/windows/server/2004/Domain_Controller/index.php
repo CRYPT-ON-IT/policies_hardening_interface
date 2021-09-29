fc3adc8e5e0406884c8d1cfc1e87fea6a970a9d4
ataumo <34421892+ataumo@users.noreply.github.com>
2021-09-29 15:29:25 +0200

    Add index in 2004 dir

--- interface/windows/server/2004/Domain_Controller/index.php
+++ interface/windows/server/2004/Domain_Controller/index.php
@@ -4,6 +4,13 @@
 <!--file that will be read in to generate csv file (original list)-->
 <?php $link_csv_original_file = "finding_list_msft_security_baseline_windows_server_2004_dc_machine.csv"; ?>
 
+<?php
+$reference_name = "Microsoft Security Baseline"; //* necessary value
+$reference_date = "09/13/2021";       // optional
+$reference_version = "1.0"; // optional
+$reference_type = "machine";    // optional
+ ?>
+
 <!-- All links -->
 <?php include($_SERVER['DOCUMENT_ROOT']."/policies_hardening_interface/ressources/links.php");?>
 

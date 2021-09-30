f5e7d1ff9eb5b25a5be69cec0207934de82dfb31
ataumo <34421892+ataumo@users.noreply.github.com>
2021-09-30 09:55:29 +0200

    Add cleaner to use space in dir name

--- ressources/dir_reader.php
+++ ressources/dir_reader.php
@@ -3,7 +3,9 @@
     $dir_enumerate = scandir(".");
     foreach ($dir_enumerate as $dir_item) {
       if (is_dir($dir_item) and $dir_item!="." and $dir_item!="..") {
-          echo "<a href=\"$actual_link$dir_item\" class=\"list-group-item list-group-item-action\">$dir_item</a>";
+        //replace %20 by space
+        $dir_item_cleaned = str_replace("%20", " ", $dir_item);
+        echo "<a href=\"$actual_link$dir_item\" class=\"list-group-item list-group-item-action\">$dir_item_cleaned</a>";
        }
     }
    ?>

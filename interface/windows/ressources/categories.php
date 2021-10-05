889897e00e811512d3ba7e34ed3c410113737d5c
ataumo <34421892+ataumo@users.noreply.github.com>
2021-10-05 19:56:08 +0200

    Update categories

--- interface/windows/ressources/categories.php
+++ interface/windows/ressources/categories.php
@@ -1,17 +1,11 @@
-<aside class="sticky-top" style="top:50px;">
-  <nav id="nav-list" class="list-group" style="border-radius: 1em;height: 80vh;overflow-y: auto;">
 <?php
-
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
 // Do not forget to include csv_parser.php before include this file.
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+?>
 
-//
-// This code will load all categories to write them in the left nav bar
-//
-
-echo $ALL_categories_content;
-
- ?>
-</nav>
+<aside class="sticky-top" style="top:50px;">
+  <nav id="nav-list" class="list-group" style="border-radius: 1em;height: 80vh;overflow-y: auto;">
+    <?php echo $ALL_categories_content;?>
+  </nav>
 </aside>

75230725d96cc938e1363b787fdc61ae914305bc
ataumo <34421892+ataumo@users.noreply.github.com>
2021-10-05 16:41:24 +0200

    Change char separator in csv-data

--- interface/windows/ressources/table_parser.php
+++ interface/windows/ressources/table_parser.php
@@ -192,8 +192,9 @@ function write_tr($data)
   }
 
   /**** Row printing ****/
+  // in this part, in csv-data, we use ,-, to separate values
   echo"
-  <tr id=\"data-$id\" class=\"$class_content $class_content_global tr-visible row-content\" csv-data=\"$id,$recommended_value,$check_box_value\" data-tags=\"$tags\" active-filter-uix=\"false\" active-filter-severity=\"false\">
+  <tr id=\"data-$id\" class=\"$class_content $class_content_global tr-visible row-content\" csv-data=\"$id,-,$recommended_value,-,$check_box_value\" data-tags=\"$tags\" active-filter-uix=\"false\" active-filter-severity=\"false\">
     <th class=\"btn-link csv-id\" scope=\"row\" data-bs-toggle=\"collapse\" data-bs-target=\"#data-content-$id_cleaned\" role=\"button\" data-target=\"#data-content-$id_cleaned\">
       <a href=\"#data-$id\" >$id</a>
     </th>
--- interface/windows/scripts/generate_csv.js
+++ interface/windows/scripts/generate_csv.js
@@ -84,7 +84,7 @@ $('#csv-generate').click(function(){
     function(i){
       var csv_data_string = $(this).attr("csv-data");
       if (csv_data_string) {
-        csv_data_sub_list = csv_data_string.split(",");
+        csv_data_sub_list = csv_data_string.split(",-,");
         csv_data_list.push(csv_data_sub_list)
       }
 
@@ -109,13 +109,13 @@ $('#csv-generate').click(function(){
 /* check event */
 $('.check-policy').change(function() {
   var data = $(this).parent().parent().attr("csv-data");
-  var csv_data_list = data.split(",");
+  var csv_data_list = data.split(",-,");
   if (csv_data_list[2]=="checked") {
     csv_data_list[2]="not-checked"
   }else {
     csv_data_list[2]="checked"
   }
-  data = csv_data_list.join(",")
+  data = csv_data_list.join(",-,")
   $(this).parent().parent().attr("csv-data",data);
 });
 
